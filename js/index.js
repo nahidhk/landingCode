b471351310a6717e8d9de7c5bf153ddde91223f9
nahidhk <nahidappltd@gmail.com>
2024-02-04 07:38:33 +0600

    update

---
+++ js/index.js
@@ -0,0 +1,19 @@
+console.log("Hello Word!");
+var img = document.getElementsByClassName("logo");
+ img[0].src="/img/logo.png";
+document.getElementById("title").style="font-size:29px;color:#FF007B;"
+
+let notetitle = `
+You can create a card with a link on our website. 
+You can use the code with your help with the code. I use this code style 
+fashion and animation!`
+//the paragraph call
+runpara()
+function runpara() {
+ /// the use for let code //
+ document.getElementById("paragraph").innerHTML=notetitle;
+ 
+}
+
+
+     
