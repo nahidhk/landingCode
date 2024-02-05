1ea9a4f183e5654e253fac50b87690a21894c563
nahidhk <nahidappltd@gmail.com>
2024-02-05 15:18:11 +0600

    hellop update

--- project/src/index.js
+++ project/src/index.js
@@ -1,12 +1,2 @@
 console.log("hello index.js");
-styleapp();
-function styleapp(){
-document.getElementById("nav").style=`
-height: 60px;
-width: 100%;
-background-color: #fff;
-position: sticky;
-top: 0;
-box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
-`
-}
+
